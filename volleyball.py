cf9bfce43c914ade3fb9a51316c5cba54ff70e6e
Owen Hankinson <owenchankinson@gmail.com>
2021-10-08 01:11:13 -0700

    few changes

--- volleyball.py
+++ volleyball.py
@@ -9,57 +9,42 @@ all_teams_dict = {}
 for i in all_combinations:
     team_overall = 0
     smteam_overall = 0
-    for j in i[0:4]:
-        person_score = data.get(j)
-        smteam_overall += person_score
-    smteam_overall = smteam_overall/4
-    team_overall += smteam_overall
-    
-    for j in i[1:5]:
-        person_score = data.get(j)
-        smteam_overall += person_score
-    smteam_overall = smteam_overall/4
-    team_overall += smteam_overall
-
-    for j in i[2:6]:
-        person_score = data.get(j)
-        smteam_overall += person_score
-    smteam_overall = smteam_overall/4
-    team_overall += smteam_overall
-
-    for j in i[3:7]:
-        person_score = data.get(j)
-        smteam_overall += person_score
-    smteam_overall = smteam_overall/4
-    team_overall += smteam_overall
-
-    for j in i[4:8]:
-        person_score = data.get(j)
-        smteam_overall += person_score
-    smteam_overall = smteam_overall/4
-    team_overall += smteam_overall
+    st = 0
+    fi = 3
+    while fi <= 7:
+        for j in i[st:fi+1]:
+            person_score = data.get(j)
+            smteam_overall += person_score
+        smteam_overall = smteam_overall/4
+        team_overall += smteam_overall
+        smteam_overall = 0
+        st += 1
+        fi += 1
 
     for j in i[5], i[6], i[7], i[0]:
         person_score = data.get(j)
         smteam_overall += person_score
     smteam_overall = smteam_overall/4
     team_overall += smteam_overall
+    smteam_overall = 0
 
     for j in i[6], i[7], i[0], i[1]:
         person_score = data.get(j)
         smteam_overall += person_score
     smteam_overall = smteam_overall/4
     team_overall += smteam_overall
+    smteam_overall = 0
 
     for j in i[7], i[0], i[1], i[2]:
         person_score = data.get(j)
         smteam_overall += person_score
     smteam_overall = smteam_overall/4
     team_overall += smteam_overall
+    smteam_overall = 0
 
     all_teams_dict[i] = team_overall/8
 
 max_key = max(all_teams_dict)
 
-print(max_key, all_teams_dict[max_key])
+print(max_key, all_teams_dict[max_key],"% Success")
 
